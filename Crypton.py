1e35c6ba0245e8e822e80a87ef1feb83c31b3830
AymanFaruk <32478632+AymanFaruk@users.noreply.github.com>
2019-01-24 19:11:13 +0300

    Add files via upload

---
+++ Crypton.py
@@ -0,0 +1,597 @@
+print("//______Crypton______//")            
+fine = []
+find = []
+style = input("1. Encryption[Enc]\n2. Decryption[Dec]\n~$ ")
+
+def _enc():
+    get = input("What do you want to Encrypt?: ")
+    print("[*] Encrypting")
+    for char in get:
+        if char == 'A':
+            val = '2.1.1'
+            fine.append(val)
+        elif char == 'a':
+            val = '2.1.2'
+            fine.append(val)
+        elif char == 'B':
+            val = '2.2.1'
+            fine.append(val)
+        elif char == 'b':
+            val = '2.2.2'
+            fine.append(val)
+        elif char == 'C':
+            val = '2.3.1'
+            fine.append(val)
+        elif char == 'c':
+            val = '2.3.2'
+            fine.append(val)
+        elif char == 'D':
+            val = '3.1.1'
+            fine.append(val)
+        elif char == 'd':
+            val = '3.1.2'
+            fine.append(val)
+        elif char == 'E':
+            val = '3.2.1'
+            fine.append(val)
+        elif char == 'e':
+            val = '3.2.2'
+            fine.append(val)
+        elif char == 'F':
+            val = '3.3.1'
+            fine.append(val)
+        elif char == 'f':
+            val = '3.3.2'
+            fine.append(val)
+        elif char == 'G':
+            val = '4.1.1'
+            fine.append(val)
+        elif char == 'g':
+            val = '4.1.2'
+            fine.append(val)
+        elif char == 'H':
+            val = '4.2.1'
+            fine.append(val)
+        elif char == 'h':
+            val = '4.2.2'
+            fine.append(val)
+        elif char == 'I':
+            val = '4.3.1'
+            fine.append(val)
+        elif char == 'i':
+            val = '4.3.2'
+            fine.append(val)
+        elif char == 'J':
+            val = '5.1.1'
+            fine.append(val)
+        elif char == 'j':
+            val = '5.1.2'
+            fine.append(val)
+        elif char == 'K':
+            val = '5.2.1'
+            fine.append(val)
+        elif char == 'k':
+            val = '5.2.2'
+            fine.append(val)
+        elif char == 'L':
+            val = '5.3.1'
+            fine.append(val)
+        elif char == 'l':
+            val = '5.3.2'
+            fine.append(val)
+        elif char == 'M':
+            val = '6.1.1'
+            fine.append(val)
+        elif char == 'm':
+            val = '6.1.2'
+            fine.append(val)
+        elif char == 'N':
+            val = '6.2.1'
+            fine.append(val)
+        elif char == 'n':
+            val = '6.2.2'
+            fine.append(val)
+        elif char == 'O':
+            val = '6.3.1'
+            fine.append(val)
+        elif char == 'o':
+            val = '6.3.2'
+            fine.append(val)
+        elif char == 'P':
+            val = '7.1.1'
+            fine.append(val)
+        elif char == 'p':
+            val = '7.1.2'
+            fine.append(val)
+        elif char == 'Q':
+            val = '7.2.1'
+            fine.append(val)
+        elif char == 'q':
+            val = '7.2.2'
+            fine.append(val)
+        elif char == 'R':
+            val = '7.3.1'
+            fine.append(val)
+        elif char == 'r':
+            val = '7.3.2'
+            fine.append(val)
+        elif char == 'S':
+            val = '7.4.1'
+            fine.append(val)
+        elif char == 's':
+            val = '7.4.2'
+            fine.append(val)
+        elif char == 'T':
+            val = '8.1.1'
+            fine.append(val)
+        elif char == 't':
+            val = '8.1.2'
+            fine.append(val)
+        elif char == 'U':
+            val = '8.2.1'
+            fine.append(val)
+        elif char == 'u':
+            val = '8.2.2'
+            fine.append(val)
+        elif char == 'V':
+            val = '8.3.1'
+            fine.append(val)
+        elif char == 'v':
+            val = '8.3.2'
+            fine.append(val)
+        elif char == 'W':
+            val = '9.1.1'
+            fine.append(val)
+        elif char == 'w':
+            val = '9.1.2'
+            fine.append(val)
+        elif char == 'X':
+            val = '9.2.1'
+            fine.append(val)
+        elif char == 'x':
+            val = '9.2.2'
+            fine.append(val)
+        elif char == 'Y':
+            val = '9.3.1'
+            fine.append(val)
+        elif char == 'y':
+            val = '9.3.2'
+            fine.append(val)
+        elif char == 'Z':
+            val = '9.4.1'
+            fine.append(val)
+        elif char == 'z':
+            val = '9.4.2'
+            fine.append(val)
+        elif char == ' ':
+            val = '0.5.3'
+            fine.append(val)
+        elif char == '0':
+            val = '0.0'
+            fine.append(val)
+        elif char == '1':
+            val = '1.13'
+            fine.append(val)
+        elif char == '2':
+            val = '2.4'
+            fine.append(val)
+        elif char == '3':
+            val = '3.4'
+            fine.append(val)
+        elif char == '4':
+            val = '4.4'
+            fine.append(val)
+        elif char == '5':
+            val = '5.4'
+            fine.append(val)
+        elif char == '6':
+            val = '6.4'
+            fine.append(val)
+        elif char == '7':
+            val = '7.5'
+            fine.append(val)
+        elif char == '8':
+            val = '8.4'
+            fine.append(val)
+        elif char == '9':
+            val = '9.5'
+            fine.append(val)
+        elif char == '_':
+            val = '0.2.5'
+            fine.append(val)
+        elif char == '+':
+            val = '0.2.7'
+            fine.append(val)
+        elif char == '-':
+            val = '0.2.8'
+            fine.append(val)
+        elif char == '=':
+            val = '0.3.2'
+            fine.append(val)
+        elif char == '*':
+            val = '0.3.1'
+            fine.append(val)
+        elif char == '/':
+            val = '0.2.3'
+            fine.append(val)
+        elif char == '~':
+            val = '0.5.1'
+            fine.append(val)
+        elif char == '`':
+            val = '0.6.1'
+            fine.append(val)
+        elif char == '!':
+            val = '0.1.4'
+            fine.append(val)
+        elif char == '<':
+            val = '0.3.5'
+            fine.append(val)
+        elif char == '>':
+            val = '0.3.6'
+            fine.append(val)
+        elif char == '?':
+            val = '0.1.3'
+            fine.append(val)
+        elif char == '.':
+            val = '0.1.1'
+            fine.append(val)
+        elif char == ',':
+            val = '0.1.2'
+            fine.append(val)
+        elif char == ':':
+            val = '0.2.4'
+            fine.append(val)
+        elif char == ';':
+            val = '0.2.6'
+            fine.append(val)
+        elif char == '|':
+            val = '0.6.2'
+            fine.append(val)
+        elif char == '[':
+            val = '0.4.5'
+            fine.append(val)
+        elif char == ']':
+            val = '0.4.6'
+            fine.append(val)
+        elif char == '{':
+            val = '0.4.7'
+            fine.append(val)
+        elif char == '}':
+            val = '0.4.8'
+            fine.append(val)
+        elif char == '"':
+            val = '0.1.6'
+            fine.append(val)
+        elif char == "'":
+            val = '0.1.5'
+            fine.append(val)
+        elif char == '@':
+            val = '0.2.1'
+            fine.append(val)
+        elif char == '$':
+            val = '0.3.8'
+            fine.append(val)
+        elif char == '&':
+            val = '0.3.4'
+            fine.append(val)
+        elif char == '%':
+            val = '0.3.3'
+            fine.append(val)
+        elif char == '(':
+            val = '0.1.7'
+            fine.append(val)
+        elif char == ')':
+            val = '0.1.8'
+            fine.append(val)
+        else:
+            print("[*] Character",char,"doesn't exist.")
+    
+def _dec():
+    get = input("What do you want to Decrypt?: ")
+    print("[*] Decrypting")
+    findl = get.split()
+    for code in findl:
+        if code == '2.1.1':
+            val = 'A'
+            find.append(val)
+        elif code == '2.1.2':
+            val = 'a'
+            find.append(val)
+        elif code == '2.2.1':
+            val = 'B'
+            find.append(val)
+        elif code == '2.2.2':
+            val = 'b'
+            find.append(val)
+        elif code == '2.3.1':
+            val = 'C'
+            find.append(val)
+        elif code == '2.3.2':
+            val = 'c'
+            find.append(val)
+        elif code == '3.1.1':
+            val = 'D'
+            find.append(val)
+        elif code == '3.1.2':
+            val = 'd'
+            find.append(val)
+        elif code == '3.2.1':
+            val = 'E'
+            find.append(val)
+        elif code == '3.2.2':
+            val = 'e'
+            find.append(val)
+        elif code == '3.3.1':
+            val = 'F'
+            find.append(val)
+        elif code == '3.3.2':
+            val = 'f'
+            find.append(val)
+        elif code == '4.1.1':
+            val = 'G'
+            find.append(val)
+        elif code == '4.1.2':
+            val = 'g'
+            find.append(val)
+        elif code == '4.2.1':
+            val = 'H'
+            find.append(val)
+        elif code == '4.2.2':
+            val = 'h'
+            find.append(val)
+        elif code == '4.3.1':
+            val = 'I'
+            find.append(val)
+        elif code == '4.3.2':
+            val = 'i'
+            find.append(val)
+        elif code == '5.1.1':
+            val = 'J'
+            find.append(val)
+        elif code == '5.1.2':
+            val = 'j'
+            find.append(val)
+        elif code == '5.2.1':
+            val = 'K'
+            find.append(val)
+        elif code == '5.2.2':
+            val = 'k'
+            find.append(val)
+        elif code == '5.3.1':
+            val = 'L'
+            find.append(val)
+        elif code == '5.3.2':
+            val = 'l'
+            find.append(val)
+        elif code == '6.1.1':
+            val = 'M'
+            find.append(val)
+        elif code == '6.1.2':
+            val = 'm'
+            find.append(val)
+        elif code == '6.2.1':
+            val = 'N'
+            find.append(val)
+        elif code == '6.2.2':
+            val = 'n'
+            find.append(val)
+        elif code == '6.3.1':
+            val = 'O'
+            find.append(val)
+        elif code == '6.3.2':
+            val = 'o'
+            find.append(val)
+        elif code == '7.1.1':
+            val = 'P'
+            find.append(val)
+        elif code == '7.1.2':
+            val = 'p'
+            find.append(val)
+        elif code == '7.2.1':
+            val = 'Q'
+            find.append(val)
+        elif code == '7.2.2':
+            val = 'q'
+            find.append(val)
+        elif code == '7.3.1':
+            val = 'R'
+            find.append(val)
+        elif code == '7.3.2':
+            val = 'r'
+            find.append(val)
+        elif code == '7.4.1':
+            val = 'S'
+            find.append(val)
+        elif code == '7.4.2':
+            val = 's'
+            find.append(val)
+        elif code == '8.1.1':
+            val = 'T'
+            find.append(val)
+        elif code == '8.1.2':
+            val = 't'
+            find.append(val)
+        elif code == '8.2.1':
+            val = 'U'
+            find.append(val)
+        elif code == '8.2.2':
+            val = 'u'
+            find.append(val)
+        elif code == '8.3.1':
+            val = 'V'
+            find.append(val)
+        elif code == '8.3.2':
+            val = 'v'
+            find.append(val)
+        elif code == '9.1.1':
+            val = 'W'
+            find.append(val)
+        elif code == '9.1.2':
+            val = 'w'
+            find.append(val)
+        elif code == '9.2.1':
+            val = 'X'
+            find.append(val)
+        elif code == '9.2.2':
+            val = 'x'
+            find.append(val)
+        elif code == '9.3.1':
+            val = 'Y'
+            find.append(val)
+        elif code == '9.3.2':
+            val = 'y'
+            find.append(val)
+        elif code == '9.4.1':
+            val = 'Z'
+            find.append(val)
+        elif code == '9.4.2':
+            val = 'z'
+            find.append(val)
+        elif code == '0.5.3':
+            val = ' '
+            find.append(val)
+        elif code == '0.0':
+            val = '0'
+            find.append(val)
+        elif code == '1.13':
+            val = '1'
+            find.append(val)
+        elif code == '2.4':
+            val = '2'
+            find.append(val)
+        elif code == '3.4':
+            val = '3'
+            find.append(val)
+        elif code == '4.4':
+            val = '4'
+            find.append(val)
+        elif code == '5.4':
+            val = '5'
+            find.append(val)
+        elif code == '6.4':
+            val = '6'
+            find.append(val)
+        elif code == '7.5':
+            val = '7'
+            find.append(val)
+        elif code == '8.4':
+            val = '8'
+            find.append(val)
+        elif code == '9.5':
+            val = '9'
+            find.append(val)
+        elif code == '0.2.5':
+            val = '_'
+            find.append(val)
+        elif code == '0.2.7':
+            val = '+'
+            find.append(val)
+        elif code == '0.2.8':
+            val = '-'
+            find.append(val)
+        elif code == '0.3.2':
+            val = '='
+            find.append(val)
+        elif code == '0.3.1':
+            val = '*'
+            find.append(val)
+        elif code == '0.2.3':
+            val = '/'
+            find.append(val)
+        elif code == '0.5.1':
+            val = '~'
+            find.append(val)
+        elif code == '0.6.1':
+            val = '`'
+            find.append(val)
+        elif code == '0.1.4':
+            val = '!'
+            find.append(val)
+        elif code == '0.3.5':
+            val = '<'
+            find.append(val)
+        elif code == '0.3.6':
+            val = '>'
+            find.append(val)
+        elif code == '0.1.3':
+            val = '?'
+            find.append(val)
+        elif code == '0.1.1':
+            val = '.'
+            find.append(val)
+        elif code == '0.1.2':
+            val = ','
+            find.append(val)
+        elif code == '0.2.4':
+            val = ':'
+            find.append(val)
+        elif code == '0.2.6':
+            val = ';'
+            find.append(val)
+        elif code == '0.6.2':
+            val = '|'
+            find.append(val)
+        elif code == '0.4.5':
+            val = '['
+            find.append(val)
+        elif code == '0.4.6':
+            val = ']'
+            find.append(val)
+        elif code == '0.4.7':
+            val = '{'
+            find.append(val)
+        elif code == '0.4.8':
+            val = '}'
+            find.append(val)
+        elif code == '0.1.6':
+            val = ''
+            find.append(val)
+        elif code == '0.1.5':
+            val = '"'
+            find.append(val)
+        elif code == '0.2.1':
+            val = "'"
+            find.append(val)
+        elif code == '0.3.8':
+            val = '$'
+            find.append(val)
+        elif code == '0.3.4':
+            val = '&'
+            find.append(val)
+        elif code == '0.3.3':
+            val = '%'
+            find.append(val)
+        elif code == '0.1.7':
+            val = '('
+            find.append(val)
+        elif code == '0.1.8':
+            val = ')'
+            find.append(val)
+        else:
+            print("[*] Code",code,"doesn't exist.")
+        
+            
+def _exit():
+    print("Wrong command.\nExiting")
+    input("Press Enter to Exit")
+
+if style == 'Enc' or style == 'enc' or style == '1':
+    print("//______Encryption______//")
+    _enc()
+    print("[*] Here's your Encryption: ",end='')
+    p = ''
+    for code in fine:
+        p += code+' '
+    print(p,'.',sep='')
+    _exit()
+elif style == 'Dec' or style == 'dec' or style == '2':
+    print("//______Decryption______//")
+    _dec()
+    print("[*] Here's your Decryption: ",end='')
+    p = ''
+    for charac in find:
+        p += charac
+    print(p,'.',sep='')
+    _exit()
+elif style != 'Dec' or style != 'Enc' or style != '1' or style != '2' or style != 'enc' or style != 'dec':
+    _exit()
